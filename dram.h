ae264aa7d1054ecbd08de96ef675bf22a4b73c9a
pascal <pascal.pw05@gmail.com>
2024-11-06 23:47:34 +0100

    setsup files

---
+++ dram.h
@@ -0,0 +1,12 @@
+#include "includes.h"
+
+#define DRAM_SIZE = 1024 * 1024 * 1;
+#define DRAM_BASE = 0x80000000
+
+typedef struct DRAM32 {
+    uint8_t mem[DRAM_SIZE];
+} DRAM32;
+
+uint32_t dram32_load(DRAM32 *dram, uint32_t addr, uint32_t size);
+void dram32_store(DRAM32 *dram, uint32_t addr, uint32_t size, uint32_t value);
+
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "includes.h"
+
+struct vCPU32 {
+    uint32_t register[32];
+    uint32_t pc;
+}
+
