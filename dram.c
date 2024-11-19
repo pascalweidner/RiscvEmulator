df00b1874fd2774f4e2633822e1d84cc64bab572
pascal <pascal.pw05@gmail.com>
2024-11-19 14:06:59 +0100

    starts documenting the code

--- dram.c
+++ dram.c
@@ -29,6 +29,10 @@ uint32_t dram32_loadU(DRAM32 *dram, uint32_t addr, uint32_t size) {
 }
 
 static uint32_t dram32_load_byte(DRAM32 *dram, uint32_t addr) {
+    assert(addr < DRAM32_BASE + DRAM_SIZE);
+    assert(dram != NULL);
+
+
     uint32_t value = *((uint8_t*)(dram->mem + addr));
     if((value >> 7)) {
         uint32_t mask = ((1 << 24) - 1) << 8;
@@ -39,6 +43,9 @@ static uint32_t dram32_load_byte(DRAM32 *dram, uint32_t addr) {
 }
 
 static uint32_t dram32_load_halfWord(DRAM32 *dram, uint32_t addr) {
+    assert(addr < DRAM32_BASE + DRAM_SIZE);
+    assert(dram != NULL);
+
     uint32_t value = *((uint16_t *)(dram->mem + addr));
     if((value >> 15)) {
         uint32_t mask = ((1 << 16) - 1) << 16;
@@ -49,6 +56,9 @@ static uint32_t dram32_load_halfWord(DRAM32 *dram, uint32_t addr) {
 }
 
 static uint32_t dram32_load_word(DRAM32 *dram, uint32_t addr) {
+    assert(addr < DRAM32_BASE + DRAM_SIZE);
+    assert(dram != NULL);   
+
     uint32_t value = *((uint32_t *)(dram->mem + addr));
     return value;
 } 
@@ -57,6 +67,13 @@ static void dram32_store_byte(DRAM32 *dram, uint32_t addr, uint8_t value);
 static void dram32_store_halfWord(DRAM32 *dram, uint32_t addr, uint16_t value);
 static void dram32_store_word(DRAM32 *dram, uint32_t addr, uint32_t value);
 
+/*
+    @param dram: pointer to a global dram instance, where the data is stored to
+    @param addr: address, to where to store the byte in the dram
+    @param size: size of the value to be stored in bits; must either be 8, 16 or 32
+    @param value: value that will be stored; only the size least significant bits are stored of value
+    @throws program exits if size is not 8, 16 or 32
+*/
 void dram32_store(DRAM32 *dram, uint32_t addr, uint32_t size, uint32_t value) {
     assert(size == 8 || size == 16 || size == 32);
     uint32_t addrMapped = addr - DRAM32_BASE;
