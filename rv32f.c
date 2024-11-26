aab81c9b251ae34e0edd390772823d883e4431b4
pascal <pascal.pw05@gmail.com>
2024-11-26 13:56:02 +0100

    starts f extension

---
+++ rv32f.c
@@ -0,0 +1,1 @@
+#include "rv32f.h"
---
+++ rv32f.h
@@ -0,0 +1,10 @@
+#ifndef RV32F_H_
+#define RV32F_H_
+#include "includes.h"
+
+#define FI_TYPE 0b0000111
+#define FR_TYPE 0b1010011
+#define FR4_TYPE 0b1000011
+#define FS_TYPE 0b0100111
+
+#endif
